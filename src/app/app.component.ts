4681672de84dd1c0c4cebacfc411dda97bf689a6
Damian Malczewski <damian.m.malczewski@gmail.com>
2021-09-09 17:30:06 +0200

    Apply prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +6,5 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 }
