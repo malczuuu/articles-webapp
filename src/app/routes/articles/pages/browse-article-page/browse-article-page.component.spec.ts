d26e5d98ecd42e22d623740f39cb8484071535df
Damian Malczewski <damian.m.malczewski@gmail.com>
2021-09-26 22:01:29 +0200

    Format code

--- src/app/routes/articles/pages/browse-article-page/browse-article-page.component.spec.ts
+++ src/app/routes/articles/pages/browse-article-page/browse-article-page.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { BrowseArticlePageComponent } from './browse-article-page.component';
 
 describe('BrowseArticlePageComponent', () => {
